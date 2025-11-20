b1acb495ca592e3424de4650cafbd66eaddcdb7e
Kaliki Karthik <kaliki.karthik@ACTCORP.LOCAL>
2025-11-20 17:04:57 +0530

    added docker compose files for each environment seperately and also added configutaion management with the help of spring cloud bus, rabbitmq, hookdeck for redirection of webhook and relevent dependencies

--- Microservices-Spring-Config/accounts/src/main/java/com/eazybank/accounts/dto/AccountsContactsInfoDto.java
+++ Microservices-Spring-Config/accounts/src/main/java/com/eazybank/accounts/dto/AccountsContactsInfoDto.java
@@ -8,8 +8,31 @@ import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import lombok.Data;
+import java.util.List;
+
+@Data
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactsInfoDto(String message, ContactInfo contactInfo, List<String> onCallSupport, String supportHours, boolean active) {
-    public static record ContactInfo(String name, String email, String linkedin) {}
+public class AccountsContactsInfoDto {
+
+    private String message;
+    private ContactInfo contactInfo;
+    private List<String> onCallSupport;
+    private String supportHours;
+    private boolean active;
+
+    @Data
+    public static class ContactInfo {
+        private String name;
+        private String email;
+        private String linkedin;
+    }
 }
 
+
+//
+//@ConfigurationProperties(prefix = "accounts")
+//public record AccountsContactsInfoDto(String message, ContactInfo contactInfo, List<String> onCallSupport, String supportHours, boolean active) {
+//    public static record ContactInfo(String name, String email, String linkedin) {}
+//}
--- Microservices-Spring-Config/cards/src/main/java/com/eazybank/cards/dto/CardsContactsInfoDto.java
+++ Microservices-Spring-Config/cards/src/main/java/com/eazybank/cards/dto/CardsContactsInfoDto.java
@@ -4,7 +4,30 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import lombok.Data;
+import java.util.List;
+
+@Data
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactsInfoDto(String message, ContactInfo contactInfo, List<String> onCallSupport, String supportHours, boolean active) {
-    public static record ContactInfo(String name, String email, String linkedin) {}
+public class CardsContactsInfoDto {
+
+    private String message;
+    private ContactInfo contactInfo;
+    private List<String> onCallSupport;
+    private String supportHours;
+    private boolean active;
+
+    @Data
+    public static class ContactInfo {
+        private String name;
+        private String email;
+        private String linkedin;
+    }
 }
+
+
+//@ConfigurationProperties(prefix = "cards")
+//public record CardsContactsInfoDto(String message, ContactInfo contactInfo, List<String> onCallSupport, String supportHours, boolean active) {
+//    public static record ContactInfo(String name, String email, String linkedin) {}
+//}
