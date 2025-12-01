2b0178cfb7869bbf3340432d82df149bc09d1291
Kaliki Karthik <karthikkaliki2000@gmail.com>
2025-12-01 19:10:28 +0530

    Implemented gateway server and service discovery and registarion changes

---
+++ Microservices-Spring-Config/accounts/src/main/java/com/eazybank/accounts/service/ICustomerService.java
@@ -0,0 +1,14 @@
+package com.eazybank.accounts.service;
+
+
+import com.eazybank.accounts.dto.CustomerDetailsDto;
+
+public interface ICustomerService {
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber,String correlationId);
+}
